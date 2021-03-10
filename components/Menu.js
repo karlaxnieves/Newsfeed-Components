aeb21d1176b1ec3df5d81522def4907e47e9149d
karlaxnieves <78055815+karlaxnieves@users.noreply.github.com>
2021-03-10 14:44:49 -0800

    working on articles.js

--- components/Menu.js
+++ components/Menu.js
@@ -20,6 +20,7 @@ let menuItems = [
 
   The 'menuMaker' takes an array of menu items as its only argument.
 
+
   Step 2: Inside the function, iterate over the array creating a list item <li> element for each item in the array.
   Add those items to the <ul>
 
@@ -31,3 +32,15 @@ let menuItems = [
 
   Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
 */
+const menuMaker = function (array) {
+  
+  array.forEach(function (li) {
+    
+  });
+  const menu = document.querySelector('.menu-button');
+
+  menu.addEventListener('click', (event) => {
+    menu.classList.toggle('menu-open');
+
+  })
+}
